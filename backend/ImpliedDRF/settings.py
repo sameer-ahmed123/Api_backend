efe464ca562706135f2f9cfdbaca1ceb0a6aed1a
sameer <sameerkhanjan123@gmail.com>
2024-03-03 00:17:59 +0500

    pgdb

--- backend/ImpliedDRF/settings.py
+++ backend/ImpliedDRF/settings.py
@@ -90,19 +90,19 @@ WSGI_APPLICATION = 'ImpliedDRF.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # # POSTGRESS DB
-# DATABASES = {
+DATABASES = {
 
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-# }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
